3c4332a7815e7786273450b510f7d47c1fc421b7
s219055009 <s219055009@mandela.ac.za>
2022-10-26 15:41:11 +0200

    initial commit

---
+++ src/com/capstone/qwirkle/Displayer.java
@@ -0,0 +1,5 @@
+package com.capstone.qwirkle;
+
+public interface Displayer<T> {
+    void display(T value);
+}
---
+++ src/com/capstone/qwirkle/Subscriber.java
@@ -0,0 +1,8 @@
+package com.capstone.qwirkle;
+
+import java.util.Map;
+
+@FunctionalInterface
+public interface Subscriber {
+    void onPublished(Object publisher, String topic, Map<String, Object> params);
+}
